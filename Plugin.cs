2eb240b9d3bad525ea04b85ba8edf12a4072e9d6
Kenny <kenny19ngo@outlook.com>
2024-01-05 22:11:49 -0500

    Suicide forces death (if possible), regardless of Godmode on. Updated algorithm to find local player.

--- Plugin.cs
+++ Plugin.cs
@@ -12,6 +12,7 @@ using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System;
 using System.Runtime.CompilerServices;
+using Unity.Netcode;
 
 namespace Godmode
 {
@@ -43,7 +44,6 @@ namespace Godmode
 
         private static bool enableGod = false;
         private static ConfigEntry<string> cfgKeyBind;
-        private static ConfigEntry<string> playerName;
         private static ConfigEntry<bool> showText;
         public static ConfigEntry<float> xOffset;
         public static ConfigEntry<float> yOffset;
@@ -63,7 +63,6 @@ namespace Godmode
             showText = Config.Bind("General", "Show Enable/Disable Text", true, "Show 'ON' or 'OFF' in the upper right corner of screen.");
             xOffset = Config.Bind("General", "X Offset", -0.09f, "Shifts the indicator text horizontally");
             yOffset = Config.Bind("General", "Y Offset", -0.025f, "Shifts the indicator text vertically");
-            playerName = Config.Bind("General", "Username", "", "Your steam username / game display name (CASE SENSITIVE)");
 
             var KeyAction = new InputAction(binding: $"<Keyboard>/{cfgKeyBind.Value}");
             KeyAction.performed += OnKeyPressed;
@@ -92,43 +91,13 @@ namespace Godmode
 
         private void OnKeyPressed2(InputAction.CallbackContext obj)
         {
-            if (enableGod)
-            {
-                mls.LogInfo("Cannot kill self, god mode is enabled");
-                return;
-            }
+            enableGod = false;
+            indicator.text = UpdateIndicatorText();
+            indicator.color = UpdateIndicatorColor();
 
-            try
-            {
-                foreach (PlayerControllerB player in Resources.FindObjectsOfTypeAll(typeof(PlayerControllerB)) as PlayerControllerB[] ?? null)
-                {
-                    if (player == null)
-                    {
-                        mls.LogInfo("Could not find any players");
-                        return;
-                    }
-
-                    mls.LogInfo($"Player username = {player.playerUsername}");
-                    if (player.playerUsername == playerName.Value)
-                    {
-                        if (!player.AllowPlayerDeath())
-                        {
-                            mls.LogInfo("Cannot kill self, you shouldn't normally die in this scenario.");
-                            return;
-                        }
-                        else
-                        {
-                            mls.LogInfo("Killing Player");
-                            player.KillPlayer(Vector3.zero, spawnBody: true, CauseOfDeath.Unknown, 1);
-                        }
-                    }
-                }
-            }
-            catch (NullReferenceException ex)
-            {
-                mls.LogError(ex.Message);
-                mls.LogError("Could not find any players.");
-            }
+            PlayerControllerB player = GameNetworkManager.Instance.localPlayerController;
+            if (!player.AllowPlayerDeath()) return;
+            player.KillPlayer(Vector3.zero, spawnBody: true, CauseOfDeath.Unknown, 1);
         }
 
         private void OnKeyPressed3(InputAction.CallbackContext obj)
